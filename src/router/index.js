a314427d77d841904b789db21b94e149aa3341fc
hchao <1595898735@qq.com>
2019-05-29 15:37:51 +0800

    no message

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,10 @@ import Index from '../view/index.vue'
 import Detail from '../view/detail.vue'
 import Comments from '../view/comments.vue'
 
+// const Index = () => import('../view/index.vue')
+// const Detail = () => import('../view/detail.vue')
+// const Comments = () => import('../view/comments.vue')
+
 Vue.use(Router);
 
 export default new Router({
